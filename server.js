af104a253faba5d4f884839c50091c579e938a49
damieb <dcognet@enseirb-matmeca.fr>
2019-11-01 18:36:57 +0100

    s

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('bienvenue sur !')
 })
 
-app.listen(3000,'192.168.0.23', function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
